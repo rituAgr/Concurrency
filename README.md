3d734cadec9bb2c9a1aa4b6c5af18f6369755b72
Ritu Agrawal <rituagrawal@Ritus-MacBook-Air.local>
2021-05-26 18:19:45 -0500

    Thread Lifecycle

--- README.md
+++ README.md
@@ -10,12 +10,34 @@ capable of running parallel independent task.
     - To know the number of core.  Goto Apple logo -> About this mac -> System Report.
 ____
 
-#### Multithreading concepts
-- There are six states of states. [Doc](https://docs.oracle.com/javase/8/docs/api/java/lang/Thread.State.html).
-    - __*Q. Does the thread being in runnable state means it is running?*__
-    - __*Ans. No, JVM handles all thread to OS to handle it. Now OS based to availablity puts the runnable state threads into running state*__
+#### Types of threads -
+1. User Thread: The main Application thread or any other thread spawned by user to do some task 
+is called User Thread.
+   * When all the applications user thread dies then the main user thread die & application stops.
+2. Daemon Thread: This is thread which is suppose to do some task in background. Monitoring, Logging
+are the kind of work they are most fit for. 
+   * So, in case of daemon thread, it dies as soon as all other user thread (except the main thread) die
+   * A thread a made daemon by setting true as follow - 
+     ```
+        Thread a = new Thread(r);
+        a.setDaemon(true);
+     ```
+____
+
+#### Thread LifeCycle
+* There are six states of states. [Doc](https://docs.oracle.com/javase/8/docs/api/java/lang/Thread.State.html)
+
+![dia](./docs/images/Thread_states.png)
+* Intersting Fact:  Does the thread being in runnable state means it is running?
+    Ans. No, JVM handles all thread to OS to handle it. Now OS based to availability puts the runnable state threads into running state   
+____
+
+#### Library Function for Thread
+  * start()
+  * join()
+  * sleep()
     
-  
+  * __*Any function or method call that causes interrupt for thread to run throws InterruptedException. This need to caught in try catch*__
 
 ___
 
@@ -24,7 +46,3 @@ ___
 - [SDK vs JDK](https://askanydifference.com/difference-between-sdk-and-jdk/#:~:text=SDK%20stands%20for%20software%20development,a%20program%20using%20java%20language.)
 
 ____
-
-#### Questions
-
-1. 
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+import static java.lang.Thread.sleep;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        List<Thread> threads = new ArrayList<>();
+
+        Runnable statusReporter = () -> {
+            try {
+                while(true){
+
+                    sleep(6000);
+                    printThread(threads);
+                }
+            } catch (InterruptedException e) {
+                System.out.println("Exception thrown ");
+            }
+
+        };
+
+        Thread statusReporterThread = new Thread(statusReporter);
+        statusReporterThread.setDaemon(true);
+        statusReporterThread.start();
 
         while(true) {
             System.out.println("Enter limit for want to check Prime number Count :");
@@ -11,20 +33,23 @@ public class Main {
             int userInput = sc.nextInt();
             if (userInput==0)
                 break;
-            Runnable r = new Runnable(){
-
-                @Override
-                public void run() {
-                    PrimeNumberUtils primeNumberUtils = new PrimeNumberUtils();
-                    int primeNumberCounts = primeNumberUtils.getPrimeNumberCounts(userInput);
-                    System.out.println("Count is " + primeNumberCounts);
-                }
+            Runnable r = () -> {
+                PrimeNumberUtils primeNumberUtils = new PrimeNumberUtils();
+                int primeNumberCounts = primeNumberUtils.getPrimeNumberCounts(userInput);
+                System.out.println("Count is " + primeNumberCounts);
             };
             //User Thread
             Thread t = new Thread(r);
-            //This makes thread as Dameon30
-            t.setDaemon(true);
+
+            threads.add(t);
+            //This makes thread as Dameon
+//            t.setDaemon(true);
             t.start();
         }
+    }
+
+    public static void printThread(List<Thread> threads){
+        System.out.println();
+        threads.forEach( thread-> System.out.print(thread.getName()+" is "+thread.getState()+";"));
     }
 }
